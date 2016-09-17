3b861fe8142b4ed0d6782081fc091187baf020da
dkurtaev <dmitry.kurtaev@gmail.com>
2016-09-17 21:45:09 +0300

    Sets separator class

---
+++ lib/include/sets_separator.hpp
@@ -0,0 +1,21 @@
+#ifndef INCLUDE_SETS_SEPARATOR_HPP_
+#define INCLUDE_SETS_SEPARATOR_HPP_
+
+#include <vector>
+
+class SetsSeparator {
+ public:
+  SetsSeparator(unsigned n_sets);
+
+  // If set is merged with others, return id of parent set.
+  unsigned GetSetDestination(unsigned set_id);
+
+  // Merge two sets.
+  void Merge(unsigned first_set_id, unsigned second_set_id);
+
+ private:
+  std::vector<unsigned> parents;
+  std::vector<unsigned> ranks;
+};
+
+#endif  // INCLUDE_SETS_SEPARATOR_HPP_
